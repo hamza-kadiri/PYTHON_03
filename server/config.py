0acfd9b5c1f3cc3c28ef24a87d147f08238f13f5
Hamza Kadiri <hamza.kadiri@student.ecp.fr>
2019-10-15 15:38:59 +0200

    Cleaned config file

--- server/config.py
+++ server/config.py
@@ -8,11 +8,11 @@ class Config(object):
     DROP_ON_INIT = True
 
     """API Config"""
-
     API_KEY = "84eae13884eb7a9e47fcc760ca08f593"
     API_URL = "https://api.themoviedb.org/3"
     THUMBNAIL_BASE_URL = "https://image.tmdb.org/t/p/w300"
-    """Auth Config"""
-    SECRET_KEY = "gjEq2dHoU3A45WhDQDGJTj3jF8MHa6oN"
     BACKDROP_BASE_URL = "https://image.tmdb.org/t/p/w1280"
     POSTER_BASE_URL = "https://image.tmdb.org/t/p/w185"
+    """Auth Config"""
+    SECRET_KEY = "gjEq2dHoU3A45WhDQDGJTj3jF8MHa6oN"
+    
